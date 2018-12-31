60d5a021ade51ff4c9bf8d8fb781bd7c1eb3328a
ashits8118 <asathish@hawk.iit.edu>
2018-12-30 19:37:13 -0600

    Initial Project Commit

---
+++ src/main/java/com/ashitsathish/springframework/springframework5petclinic/Springframework5PetclinicApplication.java
@@ -0,0 +1,14 @@
+package com.ashitsathish.springframework.springframework5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springframework5PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springframework5PetclinicApplication.class, args);
+    }
+
+}
+
